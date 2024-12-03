b11509b9f39a0122c0ed661bd2f97a13a78f1f6e
GFigoCh <gfigoch@outlook.com>
2024-12-03 22:39:00 +0700

    Finished "Add Decision Logic to your code using If Statements" course.

--- IfStatements/Program.cs
+++ IfStatements/Program.cs
@@ -4,6 +4,97 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Guided();
+        Challenge();
+    }
+
+    static void Challenge()
+    {
+        Random randomizer = new Random();
+
+        int daysUntilExpiration = randomizer.Next(12);
+        int discountPercentage;
+
+        Console.WriteLine("Days Remaining: " + daysUntilExpiration);
+
+        if (daysUntilExpiration == 0)
+        {
+            Console.WriteLine("Your subscription has expired.");
+        }
+        else if (daysUntilExpiration <= 5)
+        {
+            if (daysUntilExpiration == 1)
+            {
+                Console.WriteLine("Your subscription expires within a day!");
+                discountPercentage = 20;
+            }
+            else
+            {
+                Console.WriteLine($"Your subscription exipires in {daysUntilExpiration} days.");
+                discountPercentage = 10;
+            }
+
+            Console.WriteLine($"Renew now and save {discountPercentage}%!");
+        }
+        else if (daysUntilExpiration <= 10)
+        {
+            Console.WriteLine("Your subscription will expire soon. Renew now!");
+        }
+    }
+
+    static void Guided()
+    {
+        Random dice = new Random();
+
+        int rollOne = dice.Next(1, 7);
+        int rollTwo = dice.Next(1, 7);
+        int rollThree = dice.Next(1, 7);
+
+        /* Doubles roll test */
+        // rollOne = 6;
+        // rollTwo = 6;
+        // rollThree = 5;
+
+        /* Triples roll test */
+        // rollOne = 6;
+        // rollTwo = 6;
+        // rollThree = 6;
+
+        int total = rollOne + rollTwo + rollThree;
+
+        Console.WriteLine($"Dice Roll: {rollOne} + {rollTwo} + {rollThree} = {total}");
+
+        if ((rollOne == rollTwo) || (rollOne == rollThree) || (rollTwo == rollThree))
+        {
+            if ((rollOne == rollTwo) && (rollTwo == rollThree))
+            {
+                total += 6;
+                Console.WriteLine("Triples roll found! +6 bonus!");
+            }
+            else
+            {
+                total += 2;
+                Console.WriteLine("Doubles roll found! +2 bonus!");
+            }
+
+            Console.WriteLine("Total + Bonus: " + total);
+        }
+
+        if (total >= 16)
+        {
+            Console.WriteLine("You win a new car!");
+        }
+        else if (total >= 10)
+        {
+            Console.WriteLine("You win a new laptop!");
+        }
+        else if (total == 7)
+        {
+            Console.WriteLine("You win a trip for two!");
+        }
+        else
+        {
+            Console.WriteLine("You win a kitten!");
+        }
     }
 }
